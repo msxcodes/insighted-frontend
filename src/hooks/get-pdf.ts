aaaafae81375f23dacd33ce63c6412024c6860d6
Mohit Singh <mohitsinghx12@gmail.com>
2025-03-31 18:05:07 +0530

    created sub folder for frontend and backend

--- src/hooks/get-pdf.ts
+++
@@ -1,9 +0,0 @@
-// import { renderToStream } from '@react-pdf/renderer';
-// import APINotesPDF from '@/components/common/create-pdf';
-
-// export default async function handler(req: any, res: any) {
-//     const pdfStream = await renderToStream(<APINotesPDF />);
-//     res.setHeader('Content-Type', 'application/pdf');
-//     res.setHeader('Content-Disposition', 'attachment; filename=api_notes.pdf');
-//     pdfStream.pipe(res);
-// }
--- src/types/nav.ts
+++
@@ -1,9 +0,0 @@
-export interface NavItem {
-    name: string;
-    href: string;
-  }
-  
-  export interface NavbarProps {
-    className?: string;
-  }
-  
